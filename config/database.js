1987744afd6ac27f4e7a35e6a9fd7f7ea3e11e6c
GitProject <aniketkumarkt@gmail.com>
2019-02-24 19:57:42 +0530

    fff

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports={
-    database:'mongodb://Aniket:aniket@cluster0-shard-00-00-ocv4z.mongodb.net:27017,cluster0-shard-00-01-ocv4z.mongodb.net:27017,cluster0-shard-00-02-ocv4z.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+    database:'mongodb+srv://Aniket:aniket@cluster0-ocv4z.mongodb.net/test?retryWrites=true',
     secret:'123'
 }
